e60cfe07bb4c9ea025545166aefe2b836ffd6099
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-08-27 18:38:47 +1200

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,3 +4,13 @@
   version:1.0
   descirption: The game of rock paper scissors
 '''
+import random
+choices = ["Rock", "Paper", "Scissors"]
+def check_winner(player, computer):
+    if player == computer:
+        return "draw"
+    elif (player == "rock" and computer == "scissors") or (player == "paper" and computer == "rock") or (player == "scissors" and computer == "paper"):
+        return "player"
+    else:
+        return "computer"
+player_name = str(input("Enter your name: "))
