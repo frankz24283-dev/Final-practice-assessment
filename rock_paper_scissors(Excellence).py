27066316bec57c7ee4a560d8b478baa27316295b
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-08-29 09:47:51 +1200

    Create rock_paper_scissors(Excellence).py

---
+++ rock_paper_scissors(Excellence).py
@@ -0,0 +1,10 @@
+'''
+   author:Frank Zhang
+   date:29/08/2026
+   version:3.0
+   descirption: The game of rock paper sicssors
+'''
+import random
+MIN_ROUNDS = 1
+MAX_ROUNDS = 10
+CHOICES = ["rock", "paper", "scissors"]
