5a17c3f06e48da964c8f44ff517928a25b8c9566
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-08-30 22:12:31 +1200

    Update rock_paper_scissors(Excellence).py

--- rock_paper_scissors(Excellence).py
+++ rock_paper_scissors(Excellence).py
@@ -15,3 +15,8 @@ def get_valid_player_name():
         if len(name) > 0 :
             return name
         print("Invalid input! Name can not be empty. Please try again. \n")
+
+def get_valid_rounds():
+    while True:
+        questions_ask_user = f"How many rounds would you like to play ({MIN_ROUNDS- MAX_ROUNDS})"
+        user_input = input(questions_ask_user)
