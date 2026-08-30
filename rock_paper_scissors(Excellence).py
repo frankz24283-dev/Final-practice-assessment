88aa0e744178fb457be9323b75b4a9406d2907a4
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-08-30 22:28:45 +1200

    Update rock_paper_scissors(Excellence).py

--- rock_paper_scissors(Excellence).py
+++ rock_paper_scissors(Excellence).py
@@ -9,19 +9,19 @@ MIN_ROUNDS = 1
 MAX_ROUNDS = 10
 CHOICES = ["rock", "paper", "scissors"]
 
-def get_valid_player_name():
+def get_valid_player_name(): #Get the user name, and make sure they don't enter what I don't want. check the name
     while True:
         name = input("Please enter your name: ")
         if len(name) > 0 :
             return name
         print("Invalid input! Name can not be empty. Please try again. \n")
 
-def get_valid_rounds():
+def get_valid_rounds(): #Retrieve and verify the number of game rounds, and handle invalid data types (such as letters and null values) and boundary ranges.
     while True:
         questions_ask_user = f"How many rounds would you like to play ({MIN_ROUNDS- MAX_ROUNDS})"
         user_input = input(questions_ask_user)
 
-        if not user_input():
+        if not user_input(): #Check if the input consists entirely of numbers and block non-numeric characters.
             print("Invalid imput! Please enter a valid positive integer. \n")
         rounds = int(user_input)
 
