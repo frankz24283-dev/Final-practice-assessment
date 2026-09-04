15021748cdca56d21297ac8e13d11a91cc0871a9
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-09-05 09:19:35 +1200

    Update rock_paper_scissors(Excellence).py

--- rock_paper_scissors(Excellence).py
+++ rock_paper_scissors(Excellence).py
@@ -7,3 +7,15 @@
 import random
 game_choices = ["rock", "paper", "scissors"]
 
+def determine_round_winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return "draw"
+    if(
+        (player_choice == "rock" and computer_choice == "scissors")
+        or
+        (player_choice == "paper" and computer_choice == "rock")
+        or
+        (player_choice == "scissors" and computer_choice == "paper")
+    ):
+        return "player"
+    return "computer"
